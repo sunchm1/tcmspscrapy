459073c019c358151cdd26714be24c7ad04040af
John <sunchengming8@outlook.com>
2022-06-28 18:13:56 +0800

    ver0.1

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# coding=utf-8
-# 这是一个示例 Python 脚本。
-
-# 按 ⌃R 执行或将其替换为您的代码。
-# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print("Hi, {0}".format(name))  # 按 ⌘F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
+# @author=AmiliuS
 import scrapylib
 import pandas as pd
 
